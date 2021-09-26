45c1e2565d6339e5646925291d1bd4a3224f632b
Dergelyova <dergelevakristina@gmail.com>
2021-09-26 22:48:21 +0300

    Added folders

---
+++ HOMEWORK1/homework1.js
@@ -0,0 +1,19 @@
+// task2
+console.log("Dergelyova");
+
+// task3
+let x = 3;
+let y = 12;
+document.write(x, y);
+x = y;
+document.write(x, y);
+
+// task4
+
+const newObj = {
+    string: "Kristina",
+    number: 24,
+    boolean: true,
+    undefined: x,
+    null: null
+};
