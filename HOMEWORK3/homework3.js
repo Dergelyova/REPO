cac8ec0247adf939389b44cbe2246f1288947b0a
Dergelyova <dergelevakristina@gmail.com>
2021-10-04 15:41:39 +0300

    HW is finished

--- HOMEWORK3/homework3.js
+++ HOMEWORK3/homework3.js
@@ -1,8 +1,4 @@
-// 1. Заданий масив з елементами[2, 3, 4, 5].За допомогою циклу
-// for знайдіть добуток елементів цього масиву.Також реалізуйте завдання через
-// while.
-
-// task 1
+//***********************************task 1***********************************
 
 //1.1
 const arr = [2, 3, 4, 5];
@@ -23,8 +19,8 @@ while (i < arr.length) {
 console.log(mult);
 
 
-//task 2
 
+//***********************************task 2***********************************
 for (let i = 0; i <= 15; i++) {
     let parity;
     i % 2 === 0 ? parity = "even" : parity = "odd";
@@ -32,45 +28,58 @@ for (let i = 0; i <= 15; i++) {
 }
 
 
-//task 3
-
-// 3. Напишіть функцію randArray(k), яка заповнюватиме масив k випадковими цілими числами.Випадкові числа генеруються із діапазону 1 - 500.
-// Sample Output:
-//     randArray(5); // 399,310,232,379,40
-
-let k = [];
+//***********************************task 3***********************************
 
 function randArray(k) {
-    Math.random()
+    let arr = [];
+    while (arr.length < k) {
+        arr.push(Math.floor(Math.random() * 500) + 1);
+    }
+    console.log(arr);
+}
+
+//***********************************task 4***********************************
+
+function raiseToDegree(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        alert(Math.pow(a, b));
+    } else {
+        alert("Arguments should be integer correct numbers");
+    }
+}
+
+raiseToDegree(+prompt("What is base number?"), +prompt("What is exponent?"));
+
+
+//***********************************task 5***********************************
+
+function findMin() {
+    return Math.min(...arguments);
+}
+findMin(12, 14, 4, -4, 0.2);
+
+
+//***********************************task 6***********************************
+
+function findUnique(arr) {
+    uniqueArr = [...new Set(arr)];
+    return uniqueArr.length === arr.length;
 }
 
 
-// 4. Напишіть функцію raiseToDegree(a, b), яка повертає результат піднесення числа a до степеня b.Функція працює тільки з цілими числами.Реалізувати інтерфейс введення чисел a, b з клавіатури.
+//***********************************task 7***********************************
 
-// Sample Output:
-//     raiseToDegree(3, 4); // 81
-
-// 5. Реалізуйте функцію– findMin(), яка повинна приймати довільну кількість числових аргументів і повертати той, який має найменше значення.Порада: для розв’ язку можете скористатися спеціальним масивом arguments.
-// Sample Output:
-//     findMin(12, 14, 4, -4, 0.2); // => -4
-
-// 6. Напишіть функцію findUnique(arr), яка приймає масив arr і перевіряє на унікальність його елементи.Якщо всі елементи масиву унікальні(не мають дублів), то функція поверне true, інакше - false.
-// Sample Output:
-//     findUnique([1, 2, 3, 5, 3]); // => false
-// findUnique([1, 2, 3, 5, 11]); // => true
+function lastElem(arr, n = 1) {
+    return arr.slice(-n);
+}
 
 
+//***********************************task 8***********************************
 
-// 7. Напишіть функцію, яка повертає останній елемент масиву.Функція може приймати 2 параметра: 1 - ий масив, 2 - ий числовий параметр, що відповідає кількості 'х'
-// останніх елементів масиву, які треба вивести.
-
-// Sample Output:
-//     console.log(lastElem([3, 4, 10, -5])); // -5
-// console.log(lastElem([3, 4, 10, -5], 2)); // [10, -5]
-// console.log(lastElem([3, 4, 10, -5], 8)); // [3, 4, 10, -5]
-
-
-// 8. Напишіть функцію, яка приймає рядок як параметр і перетворює першу букву кожного слова рядка в верхній регістр.
-
-// Input string: 'i love java script'
-// Output string: 'I Love Java Script'
+function toTitleCase(str) {
+    str = str.split(" ");
+    for (let i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(" ");
+}
